f6adf5ad601b447232141ec52e66276cb03484b3
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-01-23 15:59:31 -0500

    tracking map 3.0.1v

--- animations.js
+++ animations.js
@@ -3,6 +3,10 @@ function toggleAnimation() {
     image.classList.toggle('animating');
     var image2 = document.getElementById('movingImage2')
     image2.classList.toggle('animating')
+    var image3 = document.getElementById('movingImage3')
+    image3.classList.toggle('animating')
+    var image4 = document.getElementById('movingImage4')
+    image4.classList.toggle('animating')
 }
 
 function ChangeStatus() {
@@ -11,7 +15,9 @@ function ChangeStatus() {
 
     if (currentStatus === 'ON') {
         statusElement.innerText = 'OFF';
+        statusElement.style.color = 'red';
     } else {
         statusElement.innerText = 'ON';
+        statusElement.style.color = 'white'
     }
 }
